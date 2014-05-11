f69f96f3215e57555d050af321a08c8a4b1aa0ed
ssola <sergio.sola@hellofresh.de>
2014-05-12 01:11:23 +0200

    problem 10 solved

---
+++ problem10/summation.py
@@ -0,0 +1,34 @@
+'''
+Problem 10
+https://projecteuler.net/problem=10
+'''
+import math
+
+def isPrime(num):
+    if num <= 1:
+        return False
+
+    if num == 2:
+        return True
+
+    if num%2 == 0:
+        return False
+
+    root = int(math.sqrt(num))+1
+
+    for i in xrange(3, root, 2):
+        if num % i == 0:
+            return False
+
+    return True
+
+summation = 0
+index = 0
+
+while index < 2000000:
+    index += 1
+    if(isPrime(index)):
+        summation += index
+        print index, summation
+
+print summation
